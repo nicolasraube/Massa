2c7658ab9497528eee967f8ac048e009e0db055d
nicolasraube <raubenicolas@gmail.com>
2018-01-06 18:42:48 +0100

    create react native skeleton

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.massa}>Massa</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  massa: {
+    fontSize: 24
+  },
+});
